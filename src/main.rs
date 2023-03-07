39444dce09edb81e356745afe4268b010e4d42c7
Troy Caro <iamtroy412@gmail.com>
2023-03-07 14:03:44 -0500

    Hello, Tokio!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Open a connection to the local mini-redis server.
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with a value of "tokio".
+    client.set("hello", "tokio".into()).await?;
+
+    // Get the value of the key "hello".
+    let res = client.get("hello").await?;
+
+    println!("Got value from the server: result={:?}", res);
+
+    Ok(())
 }
